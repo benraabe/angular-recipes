f871bfefced80824aa58d11cb0dc34b27f5f9a39
benjamin <benjaminraabe@unomaha.edu>
2020-01-16 22:40:31 +0100

    transfer recipe

--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, EventEmitter, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list-add',
   templateUrl: './shopping-list-add.component.html',
   styles: []
 })
-export class ShoppingListAddComponent implements OnInit {
+export class ShoppingListAddComponent implements OnInit, OnChanges {
+  @Input() selectedIngredient: Ingredient;
+  @Output() cleared = new EventEmitter();
+  isAdd = true;
 
-  constructor() { }
+  constructor(private sls: ShoppingListService) { }
 
   ngOnInit() {
   }
 
+  ngOnChanges(changes) {
+    if (changes.selectedIngredient.currentValue == null) {
+      this.selectedIngredient = { name: null, amount: null };
+      this.isAdd = true;
+    } else {
+      this.isAdd = false;
+      // this.selectedIngredient
+    }
+  }
+
+  onSubmit(form: NgForm) {
+    const newIngredient = new Ingredient(form.value.name, form.value.amount);
+    if (!this.isAdd) {
+      // Bearbeiten
+      this.sls.editIngredient(this.selectedIngredient, newIngredient);
+    } else {
+      this.sls.addIngredient(newIngredient);
+    }
+    this.onClear(form);
+  }
+
+  onClear(form: NgForm) {
+    this.cleared.emit();
+    form.resetForm();
+  }
+
+  onDelete(form: NgForm) {
+    this.sls.deleteIngredient(this.selectedIngredient);
+    this.onClear(form);
+  }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
 
@@ -10,11 +10,20 @@ import { ShoppingListService } from './shopping-list.service';
 })
 export class ShoppingListComponent implements OnInit {
   ingredients: Ingredient[];
+  selectedIngredient: Ingredient;
 
   constructor(private sls: ShoppingListService) { }
 
   ngOnInit() {
     this.ingredients = this.sls.getIngredients();
+  }
+
+  onCleared() {
+    this.selectedIngredient = null;
+  }
+
+  onSelectItem(ingredient: Ingredient) {
+    this.selectedIngredient = ingredient;
   }
 
 }
